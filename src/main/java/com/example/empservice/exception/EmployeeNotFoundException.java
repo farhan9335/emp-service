bc65cdf7616237e30edffaea31fb51f81cc0fb99
unknown <khan.farhan1985@gmail.com>
2023-10-29 11:04:56 +0530

    Added new endpoints

--- src/main/java/com/example/empservice/exception/EmployeeNotFoundException.java
+++ src/main/java/com/example/empservice/exception/EmployeeNotFoundException.java
@@ -1,13 +1,13 @@
 package com.example.empservice.exception;
 
-public class InvalidArgumentException extends RuntimeException{
+public class EmployeeNotFoundException extends RuntimeException{
 	
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 6379614151681339194L;
 
-	public InvalidArgumentException(String message) {
+	public EmployeeNotFoundException(String message) {
 		super(message);
 	}
 
